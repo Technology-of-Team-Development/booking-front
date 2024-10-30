e21706c33f567ac58a9e28890b8d1c21952a67a4
david <davidgaziev6@gmail.com>
2024-10-30 08:22:48 +0600

    add icons

---
+++ src/components/icons/MapIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="32"
+    height="33"
+    viewBox="0 0 32 33"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11.6364 23.2332L4 27.5968V10.1423L11.6364 5.77863M11.6364 23.2332L20.3636 27.5968M11.6364 23.2332V5.77863M20.3636 27.5968L28 23.2332V5.77863L20.3636 10.1423M20.3636 27.5968V10.1423M20.3636 10.1423L11.6364 5.77863"
+      stroke="currentColor"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
