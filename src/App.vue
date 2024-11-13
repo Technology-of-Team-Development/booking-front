41bdc3e11422f2d98a504e2a3a235b8ee3d1503f
david <davidgaziev6@gmail.com>
2024-11-13 11:48:25 +0600

    fix: book map

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,14 @@
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/defaultLayout.vue'
-import MapOffice from '@/components/office/MapOffice.vue'
-import { useDeskState } from '@/store/desk'
+import MapPage from '@/components/office/MapPage.vue'
 import { useGlobalState } from '@/store/global'
-import { computed } from 'vue'
 
-type Place = {
-  title: string
-  free: boolean
-}
-
-type Desk = {
-  leftPlaces: Place[]
-  rightPlaces: Place[]
-  vertical?: boolean
-}
-const useDesks = useDeskState()
-
-const topDesks: Desk[] = useDesks.getTopDesks()
-
-const bottomDesks: Desk[] = useDesks.getBottomDesks()
-
-const isActive = computed(() => useGlobalState().currentUser.value !== null)
+const currentPage = useGlobalState().currentPage
 </script>
 
 <template>
   <DefaultLayout>
-    <div
-      class="border-8 rounded-2xl border-blue-800 border-opacity-50 flex flex-col gap-32 bg-blue-50"
-    >
-      <div
-        class="flex gap-16 items-baseline py-8 px-6 border-blue-800 border-b-4 border-r-4 w-fit border-opacity-50 relative"
-      >
-        <div
-          class="absolute border-r-8 border-blue-50 w-8 h-12 bottom-2 -right-1"
-        ></div>
-        <MapOffice
-          v-for="(desk, index) in topDesks"
-          :key="index"
-          :leftPlaces="desk.leftPlaces"
-          :rightPlaces="desk.rightPlaces"
-          :active="isActive"
-        />
-      </div>
-      <div
-        class="flex relative gap-16 items-baseline border-t-4 border-r-4 w-fit border-blue-800 border-opacity-50 pt-12 pb-4 px-6"
-      >
-        <div class="absolute bg-blue-50 w-10 h-3 -top-2 right-20"></div>
-        <MapOffice
-          v-for="(desk, index) in bottomDesks"
-          :key="index"
-          :leftPlaces="desk.leftPlaces"
-          :rightPlaces="desk.rightPlaces"
-          :active="isActive"
-        />
-      </div>
-    </div>
+    <MapPage />
   </DefaultLayout>
 </template>
 
--- src/layouts/nav/AppSidebar.vue
+++ src/layouts/nav/AppSidebar.vue
@@ -15,32 +15,47 @@ import MeetingIcon from '@/components/icons/MeetingIcon.vue'
 import EmployeesIcon from '@/components/icons/EmployeesIcon.vue'
 import ReportIcon from '@/components/icons/ReportIcon.vue'
 import SettingsIcon from '@/components/icons/SettingsIcon.vue'
+import { useGlobalState } from '@/store/global'
 
 type Nav = {
   icon: Component
   title: string
+  name: 'map' | 'report' | 'employee' | 'meeting' | 'settings'
 }
 
 const navItems: Nav[] = [
-  { title: 'Карта этажа', icon: MapIcon },
-  { title: 'Совещания', icon: MeetingIcon },
-  { title: 'Сотрудники', icon: EmployeesIcon },
-  { title: 'Отчеты', icon: ReportIcon },
-  { title: 'Настройки', icon: SettingsIcon },
+  { title: 'Карта этажа', icon: MapIcon, name: 'map' },
+  { title: 'Совещания', icon: MeetingIcon, name: 'meeting' },
+  { title: 'Сотрудники', icon: EmployeesIcon, name: 'employee' },
+  { title: 'Отчеты', icon: ReportIcon, name: 'report' },
+  { title: 'Настройки', icon: SettingsIcon, name: 'settings' },
 ]
 
 const { open } = useSidebar()
+
+const currentPage = useGlobalState().currentPage
+
+function changePage(
+  newPage: 'map' | 'report' | 'employee' | 'meeting' | 'settings',
+) {
+  useGlobalState().changePage(newPage)
+}
 </script>
 
 <template>
   <Sidebar collapsible="icon">
     <SidebarContent>
       <SidebarGroup class="h-full">
-        <SidebarGroupContent class="flex flex-col mt-24">
+        <SidebarGroupContent class="flex flex-col gap-4 mt-24">
           <SidebarMenuItem v-for="navItem in navItems" :key="navItem.title">
             <SidebarMenuButton
+              @click="changePage(navItem.name)"
               size="lg"
-              class="text-lg font-bold hover:bg-blue-600 hover:text-white py-8 text-neutral-800"
+              :class="
+                navItem.name === currentPage
+                  ? 'text-lg font-bold bg-blue-600 text-white py-8 '
+                  : 'text-lg font-bold hover:bg-blue-600 hover:text-white py-8 text-neutral-800'
+              "
             >
               <component :is="navItem.icon" class="hover:text-white" />
               <span>{{ navItem.title }}</span>
