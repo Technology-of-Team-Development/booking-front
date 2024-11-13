58ce24c8f4e561c753d644c89011e52f3a9830b5
david <davidgaziev6@gmail.com>
2024-11-13 11:46:54 +0600

    add: reports

---
+++ src/components/report/ReportPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="border-8 rounded-2xl border-blue-800 border-opacity-50 flex flex-col gap-32 bg-blue-50"
+  >
+    <h1>Отчет</h1>
+  </div>
+</template>
+
+<style scoped></style>
