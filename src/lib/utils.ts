f9b0b18f06851199ca1ca3fcbb90a29df99804f7
david <davidgaziev6@gmail.com>
2024-11-24 13:33:07 +0600

    add: token funcs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const setToken = (token: string) => {
+  localStorage.setItem('token', token)
+}
+
+export const getToken = (): string | null => {
+  return localStorage.getItem('token')
+}
+
+export const removeToken = () => {
+  localStorage.removeItem('token')
+}
