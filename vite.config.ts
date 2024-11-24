03bd8c38c1722d6f153215ae9f3b5935968f0810
david <davidgaziev6@gmail.com>
2024-11-24 13:33:15 +0600

    add: proxy

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,19 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/api', // URL вашего бэкенд-сервера
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
